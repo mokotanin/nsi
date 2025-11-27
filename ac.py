119abe3bc4dbde8cb574c1f10a7ade6069466658
mokotanin <titchou68@gmail.com>
2025-11-27 19:53:02 +0100

    Chapter exercices for the test

--- ac.py
+++ ac.py
@@ -1,1 +1,14 @@
-objet=[[4,6],[6,7],[8,13],[12,16]]
+#Exo 1
+L=[4,7,12,3,9]
+print(L[0],L[4])
+L[1]=99
+L[0], L[4] = L[4], L[0]
+
+#Exo 2
+print(L)
+print([l for l in L if l % 2 == 0])
+print([l for l in L if l>=10])
+
+#Exo 3
+notes = [12, 4, 18, 9, 13]
+notes
