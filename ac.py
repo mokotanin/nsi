c65d2e6502f3ccaccca3a3a2d4ee1b1d19b257bc
mokotanin <titchou68@gmail.com>
2025-12-01 13:37:01 +0100

    S8 I

--- ac.py
+++ ac.py
@@ -1,16 +1,8 @@
-objet=[[4,6],[6,7],[8,13],[12,16]]
+from tkinter import *
+window=Tk()
+window.title("Mon Application")
+window.geometry(800,800)
+window.minsize(400,300)
+window.config(background="#0050FF")
 
-rapport=[]
-for i in range (len(objet)):
-    rapport.append(objet[i][1]/objet[i][0])
-    # input("") pour tester l'évolution de la liste
-    # print(rapport)
-print(rapport)
-rapport.sort()
-rapport.reverse()
-print(rapport)
-
-for i in range(0,len(rapport)): 
-        for j in range(0,len(rapport)): 
-            if rapport[i]==ordre[j]: 
-                indices.append(j)
+window.mainloop()
