88870deb685caf5489efba5ab90bb5e153895454
mokotanin <titchou68@gmail.com>
2025-12-12 15:08:22 +0100

    enhance exercice 6 and 8 with theme support and improved button styling; add exercice 7

--- séance 8/2. générateur de mot de passe/exercice 8.py
+++ séance 8/2. générateur de mot de passe/exercice 8.py
@@ -5,9 +5,10 @@ import tkinter as tk
 from tkinter import ttk
 import sv_ttk
 import pywinstyles, sys
+tempo=20
 
 window=Tk()
-window.title("Exo6")
+window.title("Exo8")
 window.geometry("500x400")
 window.config(background="black")
 
@@ -29,29 +30,49 @@ def apply_theme_to_titlebar(window):
 
 apply_theme_to_titlebar(window)
 
-
 def cercle():
+    global x,y,depx,depy,tempo
     r=20
-    x=randint(r,l-r)
-    y=randint(r,h-r)
-    Canvas.create_oval(x-r,y-r,x+r,y+r,outline='beige',fill='#fafafa')
+    x=x+depx
+    y=y+depy
+    if x>l-r or y<r:
+        depx=-depx
+    if y>h-r or y<r:
+        depy=-depy
+    effacer()
+    Canvas.create_oval(x-r,y-r,x+r,y+r,outline='brown',fill='#fafafa')
+    window.after(tempo,cercle)
 
 def effacer():
     Canvas.delete(ALL)
 
 l=480
 h=320
-Canvas=Canvas(window,width=l,height=h,bg="black")
+x=int(l/2)
+y=int(h/2)
+depx=randint(1,5)
+depy=randint(1,5)
+Canvas=Canvas(window,width=l,height=h,bg="#1C1C1C")
 Canvas.pack(padx=5,pady=5)
 
-style.configure("button.TButton", font=("Rubik", 16))
+def fast():
+    global tempo
+    if tempo>5:
+        tempo=tempo-5
+    else:
+        tempo=5
+def slow():
+    global tempo
+    tempo=tempo+5
+
+style.configure("button.TButton", font=("Rubik", 10))
 go=ttk.Button(window,text="Go",command=cercle,style="button.TButton")
-go.pack(side=LEFT,padx=10,pady=10,fill=X)
+go.pack(side=LEFT,fill=X)
 
-effacer=ttk.Button(window,text='Effacer',command=effacer)
-effacer.pack(side=LEFT,ipadx=15,pady=5)
+fast=ttk.Button(window,text="Plus vite",command=fast,style="button.TButton")
+fast.pack(side=LEFT,fill=X)
 
-quitter=ttk.Button(window,text='Quitter',command=quit)
-quitter.pack(side=RIGHT,padx=5)
+slow=ttk.Button(window,text="Plus doucement",command=slow,style="button.TButton")
+slow.pack(side=LEFT,fill=X)
 
 window.mainloop()
