c46b818501eb516360f91c00634d01146a10fe1a
mokotanin <titchou68@gmail.com>
2026-04-17 16:13:33 +0200

    15

--- projets/marmotte/mwa/lecteur.js
+++ projets/marmotte/mwa/lecteur.js
@@ -1,26 +1,78 @@
 const jeu = {
   0: {
-    t: "Choisissez la video de depart.",
-    c: [
-      { t: "Video 1", next: 1 },
-      { t: "Video 2", next: 2 },
-    ],
+    t: "vous êtes dans la campagne.",
+    c: [{ t: "et ?", next: 1 }],
   },
   1: {
-    t: "Vous regardez la premiere video.",
+    t: "vous allez marcher.",
     video: "rg/video.mov",
-    c: [
-      { t: "Video 2", next: 2 },
-      { t: "Video 3", next: 3 },
-    ],
+    c: [{ t: "mhm...", next: 2 }],
   },
   2: {
-    t: "Vous regardez la troisieme video.",
+    t: "vous trouvez un arbre pendant votre marche. vous voyez un trou dans la terre.",
     video: "rg/video3.mov",
+    c: [{ t: "je regarde dedans", next: 3 }],
+  },
+  3: {
+    t: "de long escaliers se trouvent dans ce trou",
+    mute: true,
     c: [
-      { t: "Video 1", next: 1 },
-      { t: "Video 3", next: 2 },
+      { t: "descendre", next: 4 },
+      { t: "ne pas y descendre", next: 99999 },
     ],
+  },
+  4: {
+    t: "ces escaliers sont très longs...",
+    video: "rg/video4.mov",
+    c: [{ t: "c'est suspect", next: 2 }],
+  },
+  5: {
+    t: "et ils continuent",
+    video: "rg/video5.mov",
+    c: [{ t: "wow", next: 6 }],
+  },
+  6: {
+    t: "une porte ???",
+    video: "rg/video6.mov",
+    c: [{ t: "ouvrir la porte", next: 7 }],
+  },
+  7: {
+    t: "qu'est-ce que-",
+    video: "rg/video7.mov",
+    c: [{ t: "une salle délabrée", next: 8 }],
+  },
+  8: {
+    t: "C'est fini. adieu. vos pensées seront retranscrites par ce texte.",
+    video: "rg/video8.mov",
+    c: [{ t: "entrez", next: 9 }],
+  },
+  9: {
+    t: "en avançant encore, j'ai trouvé une demi grille qui donnait sur un bureau",
+    video: "rg/video9.mov",
+    c: [{ t: "ramper", next: 10 }],
+  },
+  10: {
+    t: "c'est quand même fou de trouver un bureau dans cet endroit...",
+    video: "rg/video10.mov",
+    c: [{ t: "observer", next: 11 }],
+  },
+  11: {
+    video: "rg/video11.mov",
+    c: [{ t: "...", next: 12 }],
+  },
+  12: {
+    video: "rg/video12.mov", //! fonctionnement pas testé
+    c: [{ next: 13 }],
+  },
+  13: {
+    t: "",
+    video: "rg/video13.mov",
+    c: [{ t: "avancer", next: 14 }],
+  },
+  14: {
+    t: "...",
+    video: "rg/video14.mov",
+    c: [{ t: "ouvrir", next: 15 }],
   },
 }
 
@@ -102,7 +154,7 @@ function lancerVideo(scenario) {
   videoLecteur.src = scenario.video
   videoLecteur.currentTime = 0
   videoLecteur.controls = false
-  videoLecteur.muted = false
+  videoLecteur.muted = scenario.mute === true
   videoLecteur.volume = volumeUniforme
   videoLecteur.playsInline = true
   videoLecteur.load()
